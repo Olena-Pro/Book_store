007dd1ed0ce82eda9573dbefbd56ad9efd6a6d74
Olena Protsailo <olenaprotsailo@gmail.com>
2024-04-01 18:13:34 +0300

    fixed conflicts

--- src/main/java/mate/academy/bookstore/repository/book/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/book/BookRepository.java
@@ -4,6 +4,6 @@ import mate.academy.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends JpaRepository<Book,Long>, JpaSpecificationExecutor<Book> {
 
 }
